e0a1e015bbafdb5c24db46790ebd62a3a1d245c6
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-09-12 14:03:19 +0200

    vanslist

--- index.jsx
+++ index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans";
+import "./server";
 function App() {
   return (
     <BrowserRouter>
@@ -12,12 +14,14 @@ function App() {
         </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
       <footer>
         <p>Ⓒ 2022 #VANLIFE</p>
