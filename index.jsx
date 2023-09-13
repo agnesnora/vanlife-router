fa7f8a5cee34d7ade185d3a27f38ca34db80d714
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-09-13 11:50:42 +0200

    nested routes setting up host

--- index.jsx
+++ index.jsx
@@ -3,27 +3,29 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Vans from "./pages/Vans";
-import VanDetail from "./pages/VanDetail";
+import Vans from "./pages/vans/Vans";
+import VanDetail from "./pages/vans/VanDetail";
+import Layout from "./components/Layout";
 import "./server";
+import Reviews from "./pages/host/Reviews";
+import Income from "./pages/host/Income";
+import Dashboard from "./pages/host/Dashboard";
+import HostLayout from "./components/HostLayout";
+
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VanLife
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
-
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
+        </Route>
       </Routes>
       <footer>
         <p>Ⓒ 2022 #VANLIFE</p>
--- pages/vans/Vans.jsx
+++ pages/vans/Vans.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "../server";
+import "../../server";
 
 export default function Vans() {
   const [vans, setVans] = useState([]);
